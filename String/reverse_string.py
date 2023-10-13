62c213afdcd443954f4222dda477216706869941
Bishesh <bishesh.sunam@gmail.com>
2023-10-13 21:45:29 +1300

    String: reverse string two pointer

--- String/reverse_string.py
+++ String/reverse_string.py
@@ -11,3 +11,30 @@ class Solution:
         rev = arr[::-1]
         st = ' '.join(rev)
         return st
+    
+
+'''
+Question: Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+'''
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+       s[:] = s[::-1]
+
+
+
+#Two pointers
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        while(left < right):
+           s[left], s[right] = s[right], s[left]
+           left+=1
+           right-=1
+
+        return s
